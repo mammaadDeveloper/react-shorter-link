7513d4b9d043e8ca08207eebc1b50080ac01c8d9
Mohammadreza <developerMammad@proton.me>
2025-08-21 16:50:38 +0330

    feat(app, header): styling the header

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import Header from "./Header";
 import RecentLinks from "./RecentLinks";
 import ShortenBox from "./ShortenBox";
+import './styles/app.scss';
 
 function App() {
   return (
-    <div className="app">
-      <Header/>
-      <main>
-        <ShortenBox/>
-        <RecentLinks/>
-      </main>
+    <div className="app w-full h-screen flex justify-center items-center">
+      <div className="container m-2 p-6 bg-white rounded-2xl">
+        <Header />
+        <main>
+          <ShortenBox />
+          <RecentLinks />
+        </main>
+      </div>
     </div>
-  )
+  );
 }
 
 export default App;
--- src/Header.tsx
+++ src/Header.tsx
@@ -2,7 +2,7 @@ import logo from "./assets/logo.png";
 
 export default function Header() {
   return (
-    <header className="header">
+    <header className="header flex flex-col items-center">
       <img src={logo} alt="" className="header__logo" />
       <p className="header__logo-text">Link Shorter</p>
     </header>
